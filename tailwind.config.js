5c50b12320bb774ed0d2b6a670bc93a751dce099
Old <225056366+roy-henderson@users.noreply.github.com>
2026-05-16 23:05:15 -0400

    fix (#51)
    * fix next.config.js
    
    * fix .npmrc
    
    * fix app/globals.css
    
    * fix tailwind.config.js
    
    * fix lib/supabase.ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,2 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+/** @type {import("tailwindcss").Config} */
+module.exports={darkMode:["class"],content:["./app/**/*.{js,ts,jsx,tsx,mdx}","./components/**/*.{js,ts,jsx,tsx,mdx}"],theme:{extend:{colors:{border:"hsl(var(--border,214.3 31.8% 91.4%))"},borderRadius:{lg:"0.5rem"}}},plugins:[]}
